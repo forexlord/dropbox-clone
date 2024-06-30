40e26b4b44f039d0c6e0e4c590599bc706964aa1
Recovery Eyo <141423374+forexlord@users.noreply.github.com>
2024-06-30 16:09:19 +0100

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,7 @@ import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAa2i7cYtQ_HZKIfwgvmSDnqPP90QvlfeI",
-    authDomain: "reedzdropbox-clone.firebaseapp.com",
-    projectId: "reedzdropbox-clone",
-    storageBucket: "reedzdropbox-clone.appspot.com",
-    messagingSenderId: "203572528521",
-    appId: "1:203572528521:web:27d88c8ddf5a4ff92d0d58"
+   //your firebase config
   };
 
   const app = getApps().length? getApp() : initializeApp(firebaseConfig);
